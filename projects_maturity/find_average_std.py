d710c2479deb4b8242333650a958ae5613ec371b
Giannis Karyotakis <110292528+karyotakisg@users.noreply.github.com>
2025-10-13 19:30:29 +0300

    In age of projects analysis, consider that we need snapshot of 2024-12-31

--- projects_maturity/find_average_std.py
+++ projects_maturity/find_average_std.py
@@ -9,6 +9,9 @@ not_counted = 0
 for repo in repos:
     created_at = datetime.datetime.strptime(repo['createdAt'], '%Y-%m-%dT%H:%M:%S')
     updated_at = datetime.datetime.strptime(repo['lastCommit'], '%Y-%m-%dT%H:%M:%S')
+    # We care about the age of the repositories at the time of the sampling, which is at the end of 2024.
+    if updated_at.year == 2025:
+        updated_at = datetime.datetime.strptime('2024-12-31T23:59:59', '%Y-%m-%dT%H:%M:%S')
     age = updated_at - created_at
     # if age is negative, do not include in calculations. 
     # That can be attributed to GHSearch misscalculation 
@@ -19,7 +22,7 @@ for repo in repos:
     else:
         ages.append(age.days/365)
 average_age = sum(ages) / len(ages)
-print(f'Not counted repos: {not_counted}')
 std = (sum((x - average_age) ** 2 for x in ages) / len(ages)) ** 0.5
+print(f'Not counted repos: {not_counted}')
 print(f'Average age: {average_age:.2f} years')
 print(f'Standard deviation: {std:.2f} years')
